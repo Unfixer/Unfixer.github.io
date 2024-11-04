a9c5b6e01ea3635eed60beb9409f8c62814af680
Unfixer <swagnikbanerjee2004@gmail.com>
2024-11-04 15:06:39 +0530

    new portfolio

---
+++ app/_components/Footer.tsx
@@ -0,0 +1,105 @@
+import Link from "next/link";
+import { BackgroundBeams } from "./ui/BackgroundBeams";
+import ShinyButton from "./ui/ShinyButton";
+
+const Footer = () => {
+  return (
+    <div
+      className="flex flex-col justify-center antialiased relative border border-dark-300 rounded-xl mb-5"
+      id="contact"
+    >
+      <div>
+        <div className="space-y-8 p-10">
+          <h1 className="text-5xl font-bold max-w-2xl leading-[110%] relative z-10">
+            Like what you see? Reach out{" "}
+            <Link
+              href="mailto:saghnikbanerjee2004@outlook.com"
+              className="text-primary hover:text-primary/80 border-b-2 border-primary hover:border-primary/80 transition-colors duration-200"
+            >
+              via email
+            </Link>{" "}
+            to collaborate!
+          </h1>
+
+          <ShinyButton>
+            <Link href="mailto:saghnikbanerjee2004@outlook.com">Schedule call</Link>
+          </ShinyButton>
+        </div>
+
+        <div className="mt-16 p-10 border-t border-dark-200 dark:border-white/10 flex flex-col md:flex-row justify-between gap-10 md:gap-0">
+          <div className="space-y-2.5">
+            <h3 className="text-xl font-bold relative z-10">
+              Saghnik Banerjee
+            </h3>
+          </div>
+
+          <div className="flex justify-between gap-0 sm:gap-16">
+            <ul className="space-y-2.5 relative z-10 text-sm sm:text-base">
+              <li className="text-base sm:text-lg font-semibold">Navigate</li>
+              <li className="text-dark-200/60 hover:text-dark-200 dark:text-white/50 dark:hover:text-white">
+                <Link href="/">Home</Link>
+              </li>
+              <li className="text-dark-200/60 hover:text-dark-200 dark:text-white/50 dark:hover:text-white">
+                <Link href="#work">Work</Link>
+              </li>
+              <li className="text-dark-200/60 hover:text-dark-200 dark:text-white/50 dark:hover:text-white">
+                <Link href="#about">About</Link>
+              </li>
+              <li className="text-dark-200/60 hover:text-dark-200 dark:text-white/50 dark:hover:text-white">
+                <Link href="#contact">Contact</Link>
+              </li>
+            </ul>
+
+            <ul className="space-y-2.5 relative z-10 text-sm sm:text-base">
+              <li className="text-lg font-semibold">Projects</li>
+              <li className="text-dark-200/60 hover:text-dark-200 dark:text-white/50 dark:hover:text-white">
+                <Link
+                  href="https://github.com/Unfixer/ProductStore"
+                  target="_blank"
+                >
+                  Product Store
+                </Link>
+              </li>
+              <li className="text-dark-200/60 hover:text-dark-200 dark:text-white/50 dark:hover:text-white">
+                <Link
+                  href="https://github.com/Unfixer/Toralizer"
+                  target="_blank"
+                >
+                  Toralizer
+                </Link>
+              </li>
+              <li className="text-dark-200/60 hover:text-dark-200 dark:text-white/50 dark:hover:text-white">
+                <Link
+                  href="https://github.com/Unfixer/TCP-Server"
+                  target="_blank"
+                >
+                  Network Server
+                </Link>
+              </li>
+            </ul>
+
+            <ul className="space-y-2.5 relative z-10 text-sm sm:text-base">
+              <li className="text-lg font-semibold">Socials</li>
+              <li className="text-dark-200/60 hover:text-dark-200 dark:text-white/50 dark:hover:text-white">
+                <Link
+                  href="https://www.linkedin.com/in/saghnik-banerjee-903478311/"
+                  target="_blank"
+                >
+                  LinkedIn
+                </Link>
+              </li>
+              <li className="text-dark-200/60 hover:text-dark-200 dark:text-white/50 dark:hover:text-white">
+                <Link href="https://github.com/Unfixer" target="_blank">
+                  Github
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <BackgroundBeams className="hidden sm:flex" />
+    </div>
+  );
+};
+
+export default Footer;
---
+++ app/_lib/constants.ts
@@ -0,0 +1,151 @@
+export const techCardsItems = [
+  {
+    name: "Figma",
+    description: "Design Tool",
+    imageUrl: "/imgs/logos/figma-logo.svg",
+    bgColor: "bg-[#0ACF83]/20",
+  },
+  {
+    name: "TypeScript",
+    description: "JavaScript but better",
+    imageUrl: "/imgs/logos/typescript.svg",
+    bgColor: "bg-[#3178C6]/20",
+  },
+  {
+    name: "React",
+    description: "JavaScript Library",
+    imageUrl: "/imgs/logos/react.svg",
+    bgColor: "bg-[#61DAFB]/20",
+  },
+  {
+    name: "NextJS",
+    description: "React framework",
+    imageUrl: "/imgs/logos/nextjs.svg",
+    bgColor: "bg-dark-200/10 dark:bg-white/10",
+  },
+  {
+    name: "Tailwind",
+    description: "CSS framework",
+    imageUrl: "/imgs/logos/tailwind.svg",
+    bgColor: "bg-[#0EA5E9]/20",
+  },
+  {
+    name: "Git",
+    description: "Version control",
+    imageUrl: "/imgs/logos/git.svg",
+    bgColor: "bg-[#F1502F]/20",
+  },
+  {
+    name: "Python",
+    description: "Jack of All Trades",
+    imageUrl: "/imgs/logos/python.svg",
+    bgColor: "bg-[#F1502F]/20",
+  },
+  {
+    name: "C",
+    description: "???-Level",
+    imageUrl: "/imgs/logos/c.svg",
+    bgColor: "bg-[#61DAFB]/20",
+  },
+  {
+    name: "Assembly",
+    description: "Low-Level",
+    imageUrl: "/imgs/logos/asm.svg",
+    bgColor: "bg-dark-200/10 dark:bg-white/10",
+  },
+  {
+    name: "Docker",
+    description: "Containerization",
+    imageUrl: "/imgs/logos/dkr.svg",
+    bgColor: "bg-dark-200/10 dark:bg-white/10",
+  },
+  {
+    name: "Kubernetes",
+    description: "Orchestration",
+    imageUrl: "/imgs/logos/kubernetes.svg",
+    bgColor: "bg-dark-200/10 dark:bg-white/10",
+  },
+  {
+    name: "AWS",
+    description: "Cloud",
+    imageUrl: "/imgs/logos/aws.svg",
+    bgColor: "bg-[#F1502F]/20",
+  },
+  {
+    name: "Mongo DB",
+    description: "Database",
+    imageUrl: "/imgs/logos/mongodb.svg",
+    bgColor: "bg-dark-200/10 dark:bg-white/10",
+  },
+  
+ 
+];
+
+export const portfolioProjects = [
+  {
+    id: "productstoreapi",
+    heading: "Product Store API",
+    subheading: "A MERN Stack API",
+    description:
+      "This API was built to test out my abilities and revison of my MERN Stack Skills. This project employs Javascript, React, Mongo DB, Express JS, and, Node JS",
+    imageUrl: "/imgs/projects/portfolio-mockup2.png",
+    techStack: [
+      "Javascript",
+      "React",
+      "Mongo DB",
+      "Node JS",
+    ],
+    liveDemoUrl: "product-store-gray.vercel.app",
+    sourceCodeUrl: "https://github.com/Unfixer/ProductStore",
+  },
+  {
+    id: "toralizer",
+    heading: "Toralizer",
+    subheading: "Hide yourself",
+    description:
+      "This project is written in C. The program basically lets you mask your IP (not DNS) and traverse trough the TOR Network. Refer to the documentation of the project to learn more",
+    imageUrl: "/imgs/projects/portfolio-mockup1.png",
+    techStack: [
+      "C",
+      "Bash", 
+    ],
+    liveDemoUrl: "",
+    sourceCodeUrl: "https://github.com/Unfixer/Toralizer",
+  },
+  {
+    id: "webserver",
+    heading: "Network Server",
+    subheading: "Your very own Network Server in C",
+    description:
+      "This project will help understand the basics of networking and socket programming. A x86 Assembly code awaits in the future.",
+    imageUrl: "/imgs/projects/portfolio-mockup3.jpg",
+    techStack: [
+      "C",
+      "Assembly",
+     ],
+    liveDemoUrl: "",
+    sourceCodeUrl: "https://github.com/Unfixer/TCP-Server",
+  },
+];
+
+export const tooltipItems = [
+  {
+    id: 1,
+    name: "Adrish Neogi",
+    designation: "You have exceeded my expectations for your early completion of the project and quality of work",
+    image: "/imgs/avatars/Adrishneogi.jpeg",
+  },
+ 
+];
+
+export const testimonialItems = [
+  {
+    id: 1,
+    stars: 5,
+    name: "Adrish Neogi",
+    proffesion: "BBA student",
+    description:
+      "“You have exceeded my expectations for your early completion of the project and quality of work”",
+    image: "/imgs/avatars/Adrishneogi.jpeg",
+  },
+];
